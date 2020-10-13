1fd5a4a0446b10c051c09c643cd116b35ef1704b
Ctouchdev <63693926+Ctouchdev@users.noreply.github.com>
2020-10-13 15:55:08 -0500

    Updates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+// Create vars for first and last name
+// create random number from 1-10
+// say hello and random number
+
+const fName = "Cody";
+const lName = "Jones";
+const randNum = Math.ceil(Math.random(1, 10) * 10);
+
+ReactDOM.render(
+  <>
+    <h1>
+      Hello, {fName} {lName}.
+    </h1>
+    <p>Your random number of the day is: {randNum}.</p>
+  </>,
+  document.getElementById("root")
+);
